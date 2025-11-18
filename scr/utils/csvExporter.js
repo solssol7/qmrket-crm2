7302ed6cb0581ca3dcb05b72c91639b659b7dac2
solssol7 <sol@aswemake.com>
2025-11-18 16:55:58 +0900

    Create csvExporter.js

---
+++ scr/utils/csvExporter.js
@@ -0,0 +1,15 @@
+/**
+ * CSV 다운로드
+ */
+export const exportToCSV = (data, filename) => {
+  if (!window.Papa) {
+    throw new Error('Papa Parse 라이브러리가 로드되지 않았습니다');
+  }
+
+  const csv = window.Papa.unparse(data);
+  const blob = new Blob(['\uFEFF' + csv], { type: 'text/csv;charset=utf-8;' });
+  const link = document.createElement('a');
+  link.href = URL.createObjectURL(blob);
+  link.download = `${filename}_${new Date().toISOString().split('T')[0]}.csv`;
+  link.click();
+};
